3e0cb9f3742086d6e5b49c412e00df0728dfe93f
CHAOJIE XIONG <023xcj@gmail.com>
2020-10-22 18:28:51 +0800

    feat: log

--- server/middlewares/log.ts
+++ server/middlewares/log.ts
@@ -10,14 +10,15 @@ export default function log() {
         }
 
         // 接口名 用户socketId 用户id
-        console.log(`  <-- ${ctx.event}  ${ctx.socket.id} ${ctx.socket.user ? ctx.socket.user : ''}`);
+        console.log(`  <-- [${ctx.event}]  [from]${ctx.socket.id} ${ctx.socket.user ? '[to]'+ctx.socket.user : ''}`);
 
         const before = Date.now();
         await next();
         const after = Date.now();
 
         // 接口名 耗时 错误消息(如果失败了的话)
-        console.log(`  --> ${ctx.event}  ${after - before}ms ${typeof ctx.res === 'string' ? ctx.res : ''}`);
+        console.log(`  --> [${ctx.event}]  [${after - before}ms] ${typeof ctx.res === 'string' ? ctx.res : ''}`);
+        console.log("\n")
 
         return null;
     };
